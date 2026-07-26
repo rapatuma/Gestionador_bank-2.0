a0596d80ae357bcbcc980689579bf7e9b5e77795
gabo.dev <josegortegam30@gmail.com>
2026-07-26 16:59:57 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import time
 
+dicc  = {
+
+    "8298355868": " numero de Cristina Gabriela Mejia Morillo",
+    "8495225868": " numero de jose Gabriel ortega mejia",
+    "8495995868": " numero de Josefani Gabriela Ortega Mejia"
+}
+
 
 
 historial = []
@@ -7,7 +14,7 @@ historial = []
 cuenta_tercero = 123456
 cuenta_personal = 123
 balance_personal = 10000
-Primer_pin = 1234
+primer_pin = 1234
 
 name = input("ingrese su nombre: ")
 
@@ -15,27 +22,27 @@ while name == "":
     print("Debe ingresar su nombre para continuar")
     name = input("ingrese su nombre: ")
 
-log_in = int(input("ingrese su pin: "))
-time.sleep(2)
-print("verificando su pin...")
-time.sleep(2)
-while log_in != Primer_pin:
-   print("El pin es incorrecto.") 
-   log_in = int(input("ingrese su pin: "))
-   time.sleep(2)
-   print("verificando su pin...")
-   time.sleep(2)
+# log_in = int(input("ingrese su pin: "))
+# time.sleep(2)
+# print("verificando su pin...")
+# time.sleep(2)
+# while log_in != primer_pin:
+#    print("El pin es incorrecto.") 
+#    log_in = int(input("ingrese su pin: "))
+#    time.sleep(2)
+#    print("verificando su pin...")
+#    time.sleep(2)
 
-log_in_cuenta = int(input("ingrese su numero de cuenta: "))
-time.sleep(2)
-print("verificando su cuenta...")
-time.sleep(2)
-while log_in_cuenta != cuenta_personal:
-   print("El numero de cuenta es incorrecto.")
-   log_in_cuenta = int(input("ingrese su numero de cuenta: "))
-   time.sleep(2)
-   print("verificando su cuenta...")
-   time.sleep(2)
+# log_in_cuenta = int(input("ingrese su numero de cuenta: "))
+# time.sleep(2)
+# print("verificando su cuenta...")
+# time.sleep(2)
+# while log_in_cuenta != cuenta_personal:
+#    print("El numero de cuenta es incorrecto.")
+#    log_in_cuenta = int(input("ingrese su numero de cuenta: "))
+#    time.sleep(2)
+#    print("verificando su cuenta...")
+#    time.sleep(2)
 
 
 print(f"Bienvenido Sr {name} a su cuenta")
@@ -51,10 +58,9 @@ while True:
     3.- Cambiar PIN
     4.- Retirar dinero
     5.- Depositar dinero 
-    6.- Historial   
-    7- Salir del programa
-    
-
+    6.- Historial 
+    7.- Poner recargas
+    8- Salir del programa
 
 """)
     
@@ -63,15 +69,19 @@ while True:
 
     match opcion:
         case "1":
-         print("procesando...")
-         time.sleep(2)
+         print("procesando", end="", flush=True)
+         for _ in range(5):
+            time.sleep(1)
+            print(".", end="", flush=True)
          print(f" |su balance actual es: {balance_personal:.2f}| ")
          historial.append(f"Usted reviso su balance y es de: |{balance_personal:.2f}|")
         
         case "2":
-           print("Procesando....")
-           time.sleep(2)
-           cuenta1 = int(input("Introduzca la cuenta de la persona a la que le desea transferir: "))
+           print("Procesando", end="", flush=True)
+           for _ in range(5):
+             time.sleep(1.3)
+             print(".", end="", flush=True)
+           cuenta1 = int(input("| Introduzca la cuenta de la persona a la que le desea transferir: "))
            if cuenta1 == cuenta_tercero:
                print("Exelente, ya usted puede transferir")
                time.sleep(2)
@@ -92,14 +102,18 @@ while True:
         case "3":
             print("Para cambiar el pin, usted debe ingreser su pin actual")
             Pin = int(input("ingrese su pin actual:"))
-            print("Verificando pin...")
-            time.sleep(2)
-            if  Pin == Primer_pin:
+            print("Verificando pin", end="", flush=True)
+            for _ in range(5):
+                time.sleep(1.2)
+                print(".", end="", flush=True)
+            if  Pin == primer_pin:
                 print("Exelente, ahora ya usted puede cambiar su Pin") 
                 nuevo_pin = int(input("ingrese su nuevo pin: "))
-                print("Actualizando pin...")
-                time.sleep(2)
-                Primer_pin = nuevo_pin
+                print("Actualizando pin ", end="", flush=True)
+                for _ in range(5):
+                    time.sleep(2)
+                    print(".", end="", flush=True)
+                primer_pin = nuevo_pin
                 print("su pin ha sido cambiado correctamente")
                 historial.append(f"Se ha realizado un cambio de pin. El nuevo pin es: {nuevo_pin}")
             else:
@@ -108,25 +122,31 @@ while True:
 
         case "4":
             retiro = float(input("cual es la cantidad que usted desea retirar: "))
-            print("Procesando retiro....")
-            time.sleep(2)
+            print("Procesando retiro", end="", flush=True)
+            for _ in range(5):
+             time.sleep(2)
+             print(".", end="", flush=True)
             if retiro > balance_personal:
                 print("solo puedes retirar de acorde al balence de tu cuenta.")
 
             else:
                 balance_personal -= retiro
                 print("|Bien, Retirando dinero correctamente....|")
-                historial.append(f"Usted retiró {retiro} y su balance actual es: |{balance_personal}|")
+                historial.append(f"Usted retiró |{retiro} |")
 
         case "5":
             cuenta = int(input("Ingrese el numero de cuenta: "))
-            print("Validando....")
-            time.sleep(2)
+            print("Validando", end="", flush=True)
+            for _ in range(5):
+                time.sleep(1.2)
+                print(".", end="", flush=True)
             if cuenta == cuenta_personal:
                 print("Cuenta correcta, ahora puede depositar dinero")
                 depositar = int(input("Ingrese el deposito: "))
-                print("Procesando...")
-                time.sleep(2)
+                print("Procesando", end="", flush=True)
+                for _ in range(5):
+                    time.sleep(1.1)
+                    print(".", end="", flush=True)
                 balance_personal += depositar
                 print(f"|Su balence actual es de:  {balance_personal}|")
                 historial.append(f"Usted ha recibido un deposito de {depositar}")
@@ -135,11 +155,32 @@ while True:
 
 
         case "6":
-            print(f" Historial de movimientos: {historial}")
+            print(f" Historial de movimientos:| {historial}|")
 
 
         case "7":
+            recarga = input("Digite su numero de de telefono: ")
+            if recarga in dicc:
+                print(f"bien, su numero ha sido encontrado: |{dicc[recarga]}|")
+                montoRecagra = int(input("Digite el monto de la recarga: ")) 
+                if montoRecagra > balance_personal:
+                    print("No puede realizar la recarga, su balence es insuficiente")
+                else:
+                    print("Procesando recarga...")
+                    time.sleep(2)
+                    balance_personal -= montoRecagra
+                    print("Recarga realizada correctamente.")
+                    historial.append(f"Usted ha realizado una recarga al numero {recarga} de {montoRecagra}")
+        
+            else:
+                print("Numero no encontrado..")
+
+
+        case "8":
          print("Gracias por utilizar nuestros servicios... ")
          break
 
+
+        case _:
+            print("Opcion no valida, por favor seleccione una opcion valida")
         
